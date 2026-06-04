76ac73ca91671eb45ac109f9459ae8687697c4e1
clariodecrypt <clario.decrypt@gmail.com>
2026-06-04 07:57:11 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const { buildClarioPrompt } = require('./prompt');
 const { generateHTML } = require('./htmlGenerator');
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));;
 app.use(express.json());
 
 const anthropic = new Anthropic({ apiKey: process.env.ANTHROPIC_API_KEY });
